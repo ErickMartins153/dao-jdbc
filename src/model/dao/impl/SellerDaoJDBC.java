868c4d17380ce71ee4ff5ad93bf9c6f63871a403
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2023-11-11 22:07:08 -0300

    Update implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -30,7 +30,7 @@ public class SellerDaoJDBC implements SellerDao {
                     Statement.RETURN_GENERATED_KEYS);
             st.setString(1, seller.getName());
             st.setString(2,seller.getEmail());
-            st.setDate(3,new java.sql.Date(seller.getBirthdate().getTime()));
+            st.setDate(3,new java.sql.Date(seller.getBirthDate().getTime()));
             st.setDouble(4,seller.getBaseSalary());
             st.setInt(5,seller.getDepartment().getId());
 
@@ -58,7 +58,28 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void update(Seller seller) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                    "UPDATE seller "
+                            + "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+                            + "WHERE Id = ?");
 
+            st.setString(1, seller.getName());
+            st.setString(2, seller.getEmail());
+            st.setDate(3, new java.sql.Date(seller.getBirthDate().getTime()));
+            st.setDouble(4, seller.getBaseSalary());
+            st.setInt(5, seller.getDepartment().getId());
+            st.setInt(6, seller.getId());
+
+            st.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
@@ -109,7 +130,7 @@ public class SellerDaoJDBC implements SellerDao {
         seller.setName(rs.getString("Name"));
         seller.setEmail(rs.getString("Email"));
         seller.setBaseSalary(rs.getDouble("BaseSalary"));
-        seller.setBirthdate(rs.getDate("BirthDate"));
+        seller.setBirthDate(rs.getDate("BirthDate"));
         seller.setDepartment(dep);
         return seller;
     }
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -9,17 +9,17 @@ public class Seller implements Serializable {
     private Integer id;
     private String name;
     private String email;
-    private Date birthdate;
+    private Date birthDate;
     private Double baseSalary;
     private Department department;
 
     public Seller() {}
 
-    public Seller(Integer id, String name, String email, Date birthdate, Double baseSalary, Department department) {
+    public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
         this.id = id;
         this.name = name;
         this.email = email;
-        this.birthdate = birthdate;
+        this.birthDate = birthDate;
         this.baseSalary = baseSalary;
         this.department = department;
     }
@@ -48,12 +48,12 @@ public class Seller implements Serializable {
         this.email = email;
     }
 
-    public Date getBirthdate() {
-        return birthdate;
+    public Date getBirthDate() {
+        return birthDate;
     }
 
-    public void setBirthdate(Date birthdate) {
-        this.birthdate = birthdate;
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
     }
 
     public Double getBaseSalary() {
@@ -91,7 +91,7 @@ public class Seller implements Serializable {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", email='" + email + '\'' +
-                ", birthdate=" + birthdate +
+                ", birthDate=" + birthDate +
                 ", baseSalary=" + baseSalary +
                 ", department=" + department +
                 '}';
